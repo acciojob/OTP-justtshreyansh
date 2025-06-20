5dfb2577dd9db84f7c4d19ba169ebf183ff71d92
Shreyansh <149056401+justtshreyansh@users.noreply.github.com>
2025-06-20 17:46:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+const inputs = document.querySelectorAll(".code");
+
+inputs.forEach((input, index) => {
+  input.addEventListener("input", (e) => {
+    // Allow only digits
+    input.value = input.value.replace(/[^0-9]/g, '');
+
+    // Move to next input if a digit is typed
+    if (input.value && index < inputs.length - 1) {
+      inputs[index + 1].focus();
+    }
+  });
+
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      // If current input has a value, clear it
+      if (input.value) {
+        input.value = '';
+      }
+      // If it's already empty, move back and clear the previous one
+      else if (index > 0) {
+        inputs[index - 1].focus();
+        inputs[index - 1].value = '';
+      }
+    }
+  });
+});
